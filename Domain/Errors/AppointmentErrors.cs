705bf9be518bfb7c7ad893e7f72275d4c96f59f4
rzv16 <razvanboita1609@gmail.com>
2024-11-02 23:01:17 +0200

    Am schimbat patch-ul

--- Domain/Errors/AppointmentErrors.cs
+++ Domain/Errors/AppointmentErrors.cs
@@ -9,4 +9,5 @@ public static class AppointmentErrors
     public static Error GetFailed(string description) => new Error("Appointment.GetFailed", description);
     public static Error NotAllowedToCancel(string description) => new Error("Appointment.NotAllowedToCancel", description);
     public static Error CancelFailed(string description) => new Error("Appointment.CancelFailed", description);
+    public static Error AlreadyCanceled(string description) => new Error("Appointment.AlreadyCanceled", description);
 }
