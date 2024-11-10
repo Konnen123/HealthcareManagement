e353687ad7220587bfe22f0622f0dab2055f29b7
Cristian Bozomitu <cristianbozomitu202@gmail.com>
2024-11-10 13:27:44 +0200

    Added Schedule DTO's

---
+++ Application/DTOs/DailyDoctorScheduleDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Application.DTOs
+{
+    public class DailyDoctorScheduleDto
+    {
+        public Guid DailyDoctorScheduleId{ get; set; }
+        public DayOfWeek DayOfWeek { get; set; }
+        public TimeOnly StartingTime { get; set; }
+        public TimeOnly EndingTime { get; set; }
+        public int SlotDurationMinutes { get; set; }
+        public Guid DoctorId { get; set; }
+        public Guid LocationId { get; set; }
+    }
+}
