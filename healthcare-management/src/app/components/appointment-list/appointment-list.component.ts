209a7a68bd95490595c27db95ca223d859abb93e
Konnen123 <sdoaga69@yahoo.ro>
2024-11-28 09:01:22 +0200

    Upgraded angular core and cli to version 19

--- healthcare-management/src/app/components/appointment-list/appointment-list.component.ts
+++ healthcare-management/src/app/components/appointment-list/appointment-list.component.ts
@@ -3,11 +3,10 @@ import {AppointmentService} from '../../services/appointment/appointment.service
 import {Appointment} from '../../models/appointment.model';
 
 @Component({
-  selector: 'app-appointment-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './appointment-list.component.html',
-  styleUrl: './appointment-list.component.scss'
+    selector: 'app-appointment-list',
+    imports: [],
+    templateUrl: './appointment-list.component.html',
+    styleUrl: './appointment-list.component.scss'
 })
 export class AppointmentListComponent implements OnInit
 {
