2bad58f383a6319fdef0e774d6ea43042d5f09d9
Cristian Bozomitu <cristianbozomitu202@gmail.com>
2024-12-06 00:09:07 +0200

    Identity-Added identity project

---
+++ Identity/DependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Repositories;
+using Identity.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Identity
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddIdentity(this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            //To add connection string for DB
+
+            string jwtSecret = configuration["Jwt:Key"];
+            Console.WriteLine("SECRET:" +  jwtSecret);
+            var key = Encoding.ASCII.GetBytes(jwtSecret);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+            });
+
+            services.AddScoped<IUsersRepository, UsersRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ Identity/Repositories/UsersRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Domain.Utils;
+using Microsoft.Extensions.Configuration;
+
+namespace Identity.Repositories
+{
+    public class UsersRepository : IUsersRepository
+    {
+        //private readonly UsersDbContext usersDbContext;
+        private readonly IConfiguration configuration;
+
+        public UsersRepository(IConfiguration configuration)
+        {
+           // this.usersDbContext = usersDbContext;
+            this.configuration = configuration;
+        }
+
+        public async Task<Result<string>> Login(User user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Result<Guid>> Register(User user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
