7225ccc9c59637f73478260f73c75014c4dfa2ee
Konnen123 <sdoaga69@yahoo.ro>
2024-12-15 15:34:51 +0200

    Added GetAllDoctors api backend

---
+++ HealthcareManagement/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Application.Use_Cases.Queries.UserQueries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthcareManagement.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [Authorize(Policy = "DOCTOR_PATIENT")]
+        [HttpGet("Doctors")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public async Task<IActionResult> GetAllDoctors()
+        {
+            var result = await mediator.Send(new GetAllDoctorsQuery());
+            return result.Match<IActionResult>(
+                onSuccess: value => Ok(value),
+                onFailure: error => BadRequest(error)
+            );
+        }
+    }
+}
